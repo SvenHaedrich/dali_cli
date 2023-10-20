6e1f054f6ca1a637abe53a09c72134f9486da76e
SvenHaedrich <sven.haedrich@sevenlab.de>
2023-10-20 11:49:28 +0200

    Switch to new API of dali_interface

--- dali/DALI/gear/clear.py
+++ dali/DALI/gear/clear.py
@@ -10,21 +10,21 @@ def clear():
     write_gear_frame(SpecialCommandOpcode.INITIALISE, send_twice=True, block=True)
     set_dtr0(0xFF)
     write_gear_frame(SpecialCommandOpcode.DTR0, opcode_byte=0xFF)
-    dali.connection.get_next(dali.timeout_sec)
+    dali.connection.get(dali.timeout_sec)
     if not dali.connection.data == dali.connection.last_transmit:
         click.echo("transmit DTR0 failed.")
     address = "BC"
     gear_send_forward_frame(address, ConfigureCommandOpcode.SET_SHORT_ADR, True)
-    dali.connection.get_next(dali.timeout_sec)
-    dali.connection.get_next(dali.timeout_sec)
+    dali.connection.get(dali.timeout_sec)
+    dali.connection.get(dali.timeout_sec)
     if not dali.connection.data == dali.connection.last_transmit:
         click.echo("transmit SET SHORT ADDRESS failed.")
     for group in range(dali.MAX_GROUP):
         gear_send_forward_frame(
             address, (ConfigureCommandOpcode.REMOVE_GROUP + group), True
         )
-        dali.connection.get_next(dali.timeout_sec)
-        dali.connection.get_next(dali.timeout_sec)
+        dali.connection.get(dali.timeout_sec)
+        dali.connection.get(dali.timeout_sec)
         if not dali.connection.data == dali.connection.last_transmit:
             click.echo(f"transmit REMOVE GROUP {group} failed.")
     write_gear_frame(SpecialCommandOpcode.TERMINATE)
