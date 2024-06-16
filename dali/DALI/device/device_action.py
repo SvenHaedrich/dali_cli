20e1966b52f73d3a101464c25b0025fe3439057b
Sven Hädrich <sven.haedrich@sevenlab.de>
2024-06-16 14:16:38 +0200

    start all the device stuff

---
+++ dali/DALI/device/device_action.py
@@ -0,0 +1,108 @@
+import click
+import dali
+import logging
+from typeguard import typechecked
+
+
+from .device_address import DaliDeviceAddressByte
+from .device_opcode import DeviceSpecialCommandOpcode
+
+from ..dali_interface.dali_interface import DaliFrame
+
+
+logger = logging.getLogger(__name__)
+
+
+# @typechecked
+# def gear_send_forward_frame(
+#     adr_parameter: str, opcode: int, send_twice: bool = False, close: bool = True
+# ):
+#     logger.debug("gear_send_forward_frame")
+#     address = DaliAddressByte()
+#     if address.arg(adr_parameter):
+#         command = address.byte << 8 | opcode
+#         dali.connection.transmit(
+#             DaliFrame(length=16, data=command, send_twice=send_twice)
+#         )
+#     else:
+#         raise click.BadOptionUsage("adr", "invalid address option.")
+#     if close:
+#         dali.connection.close()
+
+
+@typechecked
+def query_device_value(adr_parameter: str, opcode: int, close: bool = True) -> int | None:
+    logger.debug("gear_query_value")
+    address = DaliDeviceAddressByte()
+    instance = 0xFE
+    if address.arg(adr_parameter):
+        command = (address.byte << 16) | (instance << 8) | opcode
+        reply = dali.connection.query_reply(DaliFrame(length=24, data=command))
+        if reply.length == 8:
+            return reply.data
+    else:
+        raise click.BadOptionUsage("adr", "invalid address option.")
+    if close:
+        dali.connection.close()
+    return None
+
+
+# @typechecked
+# def gear_query_and_display_reply(
+#     adr_parameter: str, opcode: int, close: bool = True
+# ) -> None:
+#     logger.debug("gear_query_and_display_reply")
+#     address = DaliAddressByte()
+#     address.arg(adr_parameter)
+#     command = address.byte << 8 | opcode
+#     reply = dali.connection.query_reply(DaliFrame(length=16, data=command))
+#     if reply.length == 8:
+#         click.echo(f"0x{reply.data:02X} = " f"{reply.data} = " f"{reply.data:08b}b")
+#     else:
+#         click.echo(reply.message)
+#     if close:
+#         dali.connection.close()
+
+
+@typechecked
+def set_device_dtr0(value: int, parameter_hint: str = "UNKNOWN") -> None:
+    logger.debug("set_device_dtr0")
+    if 0 <= value < dali.MAX_VALUE:
+        command = 0xC1 << 16 | DeviceSpecialCommandOpcode.DTR0 << 8 | value
+        dali.connection.transmit(DaliFrame(length=24, data=command), block=True)
+    else:
+        raise click.BadParameter(
+            "needs to be between 0 and 255.", param_hint=parameter_hint
+        )
+
+
+@typechecked
+def set_device_dtr1(value: int, parameter_hint: str = "UNKNOWN") -> None:
+    logger.debug("set_device_dtr1")
+    if 0 <= value < dali.MAX_VALUE:
+        command = 0xC1 << 16 | DeviceSpecialCommandOpcode.DTR1 << 8 | value
+        dali.connection.transmit(DaliFrame(length=24, data=command), block=True)
+    else:
+        raise click.BadParameter(
+            "needs to be between 0 and 255.", param_hint=parameter_hint
+        )
+
+
+# @typechecked
+# def write_gear_frame(
+#     address_byte: int, opcode_byte: int = 0, send_twice: bool = False
+# ) -> None:
+#     logger.debug("write_gear_frame")
+#     command = address_byte << 8 | opcode_byte
+#     dali.connection.transmit(DaliFrame(length=16, data=command, send_twice=send_twice))
+
+
+# @typechecked
+# def write_frame_and_show_answer(address_byte: int, opcode_byte: int = 0):
+#     logger.debug("write_frame_and_show_answer")
+#     data = address_byte << 8 | opcode_byte
+#     reply = dali.connection.query_reply(DaliFrame(length=16, data=data))
+#     if reply.length == 8:
+#         click.echo(f"{reply.data} = " f"0x{reply.data:02X} = " f"{reply.data:08b}b")
+#     else:
+#         click.echo(reply.message)
---
+++ dali/DALI/device/device_address.py
@@ -0,0 +1,77 @@
+from typing import Final
+from typeguard import typechecked
+
+import dali
+
+
+@typechecked
+class DaliDeviceAddressByte:
+    SHORT: Final[str] = "SHORT"
+    GROUP: Final[str] = "GROUP"
+    BROADCAST: Final[str] = "BROADCAST"
+    UNADDRESSED: Final[str] = "UNADDRESSED"
+    SPECIAL: Final[str] = "SPECIAL"
+    RESERVED: Final[str] = "RESERVED"
+    INVALID: Final[str] = "INVALID"
+
+    def __init__(self) -> None:
+        self.byte = 0x01
+        self.mode = self.INVALID
+
+    def short(self, address: int = 0) -> None:
+        if 0 < address < dali.MAX_ADR:
+            self.byte = 0x01
+            self.byte |= address << 1
+            self.mode = self.SHORT
+
+    def group(self, group: int = 0) -> None:
+        if 0 < group < dali.MAX_GROUP:
+            self.byte = group << 1
+            self.byte |= 0x81
+            self.mode = self.GROUP
+
+    def broadcast(self) -> None:
+        self.byte = 0xFF
+        self.mode = self.BROADCAST
+
+    def unaddressed(self) -> None:
+        self.byte = 0xFD
+        self.mode = self.UNADDRESSED
+
+    def special(self, code: int = 0) -> None:
+        self.byte &= 0x01
+        if 0 < code < 16:
+            self.byte = code << 1
+            self.byte |= 0xC1
+            self.mode = self.SPECIAL
+
+    def arg(self, text: str = "") -> bool:
+        text = text.upper()
+        if text == "BC":
+            self.broadcast()
+            return True
+        if text == "BCU":
+            self.unaddressed()
+            return True
+        if text[0] == "G":
+            g = int(text[1:3])
+            if g in range(16):
+                self.group(g)
+                return True
+            else:
+                return False
+        s = int(text)
+        if 0 < s < dali.MAX_ADR:
+            self.short(s)
+            return True
+        return False
+
+    def __str__(self) -> str:
+        if self.mode == self.SHORT:
+            short_address = (self.byte >> 1) & 0x3F
+            return f"D{short_address:02}"
+        elif self.mode == self.GROUP:
+            group_address = (self.byte >> 1) & 0xF
+            return f"DG{group_address:02}"
+        else:
+            return self.mode
