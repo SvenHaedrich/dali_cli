749bf0b3666e61fe8d91ac4cb2c5f3afa3f7b854
Sven Hädrich <sven.haedrich@sevenlab.de>
2026-02-04 19:27:53 +0100

    Use predefined methods for CLEAR command

--- dali/DALI/gear/gear_clear.py
+++ dali/DALI/gear/gear_clear.py
@@ -5,11 +5,11 @@ from dali_interface import DaliInterface
 
 from ..system.constants import DaliMax, DaliTimeout
 from .gear_action import (
-    gear_send_forward_frame,
     set_gear_dtr0,
     write_gear_frame,
     write_gear_frame_and_wait,
 )
+from .gear_address import GearAddress
 from .gear_opcode import GearConfigureCommandOpcode, GearSpecialCommandOpcode
 
 
@@ -18,17 +18,9 @@ from .gear_opcode import GearConfigureCommandOpcode, GearSpecialCommandOpcode
 def clear(dali: DaliInterface):
     write_gear_frame(dali, GearSpecialCommandOpcode.INITIALISE, send_twice=True)
     set_gear_dtr0(dali, 0xFF)
-    write_gear_frame_and_wait(dali, GearSpecialCommandOpcode.DTR0, opcode_byte=0xFF)
-    address = "BC"
-    gear_send_forward_frame(dali, address, GearConfigureCommandOpcode.SET_SHORT_ADDRESS, True)
-    dali.get(DaliTimeout.DEFAULT.value)
-    dali.get(DaliTimeout.DEFAULT.value)
-    if not dali.data == dali.last_transmit:
-        click.echo("transmit SET SHORT ADDRESS failed.")
-    for group in range(DaliMax.GROUP):
-        gear_send_forward_frame(dali, address, (GearConfigureCommandOpcode.REMOVE_GROUP + group), True)
-        dali.get(DaliTimeout.DEFAULT.value)
-        dali.get(DaliTimeout.DEFAULT.value)
-        if not dali.data == dali.last_transmit:
-            click.echo(f"transmit REMOVE GROUP {group} failed.")
-        write_gear_frame(dali, GearSpecialCommandOpcode.TERMINATE)
+    address = GearAddress()
+    address.arg("BC")
+    write_gear_frame_and_wait(dali, address.byte, GearConfigureCommandOpcode.SET_SHORT_ADDRESS, send_twice=True)
+    for group in range(DaliMax.GEAR_GROUP):
+        write_gear_frame_and_wait(dali, address.byte, (GearConfigureCommandOpcode.REMOVE_GROUP + group), True)
+    write_gear_frame(dali, GearSpecialCommandOpcode.TERMINATE)
